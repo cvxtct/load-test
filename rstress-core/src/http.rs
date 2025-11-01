c57593264ba2470a7d7e1d5c52927f7a0e1cff6e
ablz <ifearnoghosts@gmail.com>
2025-11-01 10:07:34 +0100

    Update http.rs
    Count timeouts

--- rstress-core/src/http.rs
+++ rstress-core/src/http.rs
@@ -6,12 +6,10 @@ use crate::config::Config;
 
 pub fn build_client(cfg: &Config) -> Result<Client> {
     let timeout = humantime::parse_duration(&cfg.timeout)?;
-    let pool_max_idle = &cfg.pool_max_idle;
-    let pool_idle_timeout = &cfg.pool_idle_timeout;
     Ok(Client::builder()
-        .pool_max_idle_per_host(*pool_max_idle) // Disable connection reuse
+        .pool_max_idle_per_host(cfg.pool_max_idle)
+        .pool_idle_timeout(Duration::from_secs(cfg.pool_idle_timeout))
         .timeout(timeout)
         .danger_accept_invalid_certs(!cfg.verify_tls)
-        .pool_idle_timeout(Duration::from_secs(*pool_idle_timeout))
         .build()?)
 }
