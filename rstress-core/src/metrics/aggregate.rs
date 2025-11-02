c900396d7ceae7be4223e1c698dd990f07372d74
ablz <ifearnoghosts@gmail.com>
2025-11-02 07:08:24 +0100

    Use is_empty() instead of len() == 0 for Histogram
    Add idiomatic Rust style in quantile_ms.

--- rstress-core/src/metrics/aggregate.rs
+++ rstress-core/src/metrics/aggregate.rs
@@ -2,6 +2,6 @@ use hdrhistogram::Histogram;
 
 #[inline]
 pub fn quantile_ms(hist: &Histogram<u64>, q: f64) -> f64 {
-    if hist.len() == 0 { return 0.0; }
+    if hist.is_empty() { return 0.0; }
     hist.value_at_quantile(q / 100.0) as f64 / 1000.0
 }
