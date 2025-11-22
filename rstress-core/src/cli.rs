b558c6d6e3c2a58231760a4056f6c34afe80bfd8
ablz <ifearnoghosts@gmail.com>
2025-11-22 10:17:59 +0100

    Update cli.rs
    Formatting

--- rstress-core/src/cli.rs
+++ rstress-core/src/cli.rs
@@ -2,7 +2,11 @@ use clap::{ArgGroup, Parser};
 use std::path::PathBuf;
 
 #[derive(Debug, Parser, Clone)]
-#[command(name = "rstress", version, about = "Tiny HTTP load tester with async + multiprocess")]
+#[command(
+    name = "rstress",
+    version,
+    about = "Tiny HTTP load tester with async + multiprocess"
+)]
 #[command(group(
     ArgGroup::new("runmode")
         .args(["worker"])
